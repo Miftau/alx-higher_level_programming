34f8d45a35a571c86d358f982788e3a3b1cab5a0
Miftau <miftaudeenb@gmail.com>
2024-02-22 01:47:57 +0100

    files modified

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,8 +14,24 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
-        dictionary representation of a Student instance
+        dictionary representation of a Student instance with attrs validation
         """
+        if (type(attrs) is list):
+            new_dict = {}
+            my_dictv = vars(self)
+            set_dict = set(my_dictv)
+            set_list = set(attrs)
+            keys = list(set_dict & set_list)
+            for key in keys:
+                new_dict[key] = my_dictv[key]
+            return (new_dict)
         return vars(self)
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for (key, value) in json.items():
+            setattr(self, key, value)
